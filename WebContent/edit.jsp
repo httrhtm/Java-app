b337548d19ad05abb45cb41fe01ed3e78ed59dd8
httrhtm <h_hattori@emyus.jp>
2021-02-26 16:25:49 +0900

    編集画面で問題番号と問題を表示

--- WebContent/edit.jsp
+++ WebContent/edit.jsp
@@ -1,5 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" import="bean.*,java.util.*"%>
+<%@ page import="bean.QuestionsBean" %>
+<%
+List<CorrectAnswersBean> calist=(List<CorrectAnswersBean>)request.getAttribute("calist");
+String error = (String)request.getAttribute("error");
+%>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,12 +21,25 @@
 	</div>
 	<form action="EditConfirmServlet" method="post">
 		<div class="main">
+<%
+//questionは1つだけ取るからforでたくさん取る必要なし
+//リストデータをリクエストから取得
+
+QuestionsBean qbean= (QuestionsBean)request.getAttribute("qbean");
+%>
 			<div class="questionNum">
-				<p>問題番号:<input type="text" id="questionNum" name="questionNum" value=""></p>
+				<p>問題番号:
+					<input type="text" id="questionNum" name="questionNum" value="<%= qbean.getId() %>">
+				</p>
 			</div>
 			<div class="question">
-				<p>問題:<textarea id="question" name="question"></textarea></p>
+				<p>問題:
+					<textarea id="question" name="question">
+						<%= qbean.getQuestion() %>
+					</textarea>
+				</p>
 			</div>
+
 			<div class="answer">
 				<p>
 					答え:
---
+++ src/servlet/EditServlet.java
@@ -0,0 +1,83 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.QuestionsBean;
+import dao.QuestionsDao;
+
+/**
+ * Servlet implementation class EditServlet
+ */
+@WebServlet("/EditServlet")
+public class EditServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String error = "";
+		request.setCharacterEncoding("UTF-8");
+
+//questionは1つだけ使うからListを使う必要がない＝beanから直接持ってくる
+//answerは問題に対して答えが複数あるから配列で取る
+
+		//パラメータの取得
+		String question_id = request.getParameter("questionId");
+
+		try {
+			//インスタンスを生成（変数でクラスにアクセス）
+			QuestionsDao qdao = new QuestionsDao();
+			QuestionsBean qbean = new QuestionsBean();
+
+			// intに変換して取得
+			int que_id = Integer.parseInt(question_id);
+
+			//メソッドを呼び出し
+			qbean = qdao.find(que_id); //findの引数はint
+
+			// DBに一致するものがあるかどうか判定
+			if (question_id != null) {
+
+				//あったらリクエストスコープに登録
+				request.setAttribute("qbean", qbean);
+
+				request.getRequestDispatcher("edit.jsp").forward(request,response);
+			}
+		}catch(SQLException e) {
+            error ="DB接続エラーの為、一覧表示はできませんでした。";
+        }catch(Exception e){
+            error ="予期せぬエラーが発生しました。<br>"+e;
+        }finally{
+        	request.setAttribute("error", error);
+        	request.getRequestDispatcher("list.jsp").forward(request,response);
+	    }
+
+		doGet(request, response);
+	}
+
+}
