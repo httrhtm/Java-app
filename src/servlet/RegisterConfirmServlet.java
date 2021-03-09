6dda7373aa9e24a7605240b9d7adcbf9513c459e
httrhtm <h_hattori@emyus.jp>
2021-03-09 19:35:37 +0900

    問題と答えの文字数を制限する処理を追加

--- src/servlet/RegisterConfirmServlet.java
+++ src/servlet/RegisterConfirmServlet.java
@@ -16,52 +16,55 @@ import javax.servlet.http.HttpServletResponse;
 public class RegisterConfirmServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterConfirmServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-
+	public RegisterConfirmServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-				//register.jspの入力値をregisterConfirm.jspで表示
+		//register.jspの入力値をregisterConfirm.jspで表示
 		request.setCharacterEncoding("UTF-8");
 		String question = request.getParameter("question");
 		String[] answer = request.getParameterValues("answer");
 
-		if(isEmpty(question) || answer.length == 0){
+		if(isEmpty(question)){
 			//入力値が空だったら戻す
-			request.setAttribute("error_message", "問題と回答を入力してください");
+			request.setAttribute("error_msg", "問題を入力してください");
 			RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
 			rd.forward(request, response);
-		}else if(question.length() > 255) {
-			request.setAttribute("error_message", "文字数が多いです");
+		}else if(question.length() > 500) {
+			request.setAttribute("error_msg", "問題の文字数が500文字を超えています");
 			RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
-
-			rd.forward(request, response);
-		}else {
-			//入力値をsetAttribute()で登録
-			request.setAttribute("question", question);
-			request.setAttribute("answer", answer);
-
-			RequestDispatcher rd = request.getRequestDispatcher("registerConfirm.jsp");
-
 			rd.forward(request,response);
+
+		}else{
+			//answerの配列の長さ分、ループ処理
+			for(int j=0;j<answer.length;j++){
+				if(answer[j].length() == 0) {
+					request.setAttribute("error_msg", "答えを入力してください");
+					RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
+					rd.forward(request, response);
+				//answerが200文字以上だった場合
+				}else if (answer[j].length() > 200) {
+					request.setAttribute("error_msg", "答えの文字数が200文字を超えています");
+					RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
+					rd.forward(request,response);
+				}else {
+					//入力値をsetAttribute()で登録
+					request.setAttribute("question", question);
+					request.setAttribute("answer", answer);
+					RequestDispatcher rd = request.getRequestDispatcher("registerConfirm.jsp");
+					rd.forward(request,response);
+				}
+			}
 		}
-            }
+	}
 
 	private boolean isEmpty(String question) {
 		// TODO 自動生成されたメソッド・スタブ
