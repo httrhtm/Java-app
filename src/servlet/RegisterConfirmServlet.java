84daae7c9ae3ed1d2413e0d6be2f196a566242df
httrhtm <h_hattori@emyus.jp>
2021-03-09 20:04:25 +0900

    ログインしていない場合の処理を追加

--- src/servlet/RegisterConfirmServlet.java
+++ src/servlet/RegisterConfirmServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class RegisterServlet
@@ -30,37 +31,55 @@ public class RegisterConfirmServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//register.jspの入力値をregisterConfirm.jspで表示
 		request.setCharacterEncoding("UTF-8");
-		String question = request.getParameter("question");
-		String[] answer = request.getParameterValues("answer");
+		//HttpServletRequest.getSession()メソッドを呼び出しHttpSessionを取得
+		HttpSession session = request.getSession(false);
+		//sessionがnullだった場合、login画面へ遷移
+		if (session == null) {
+			session = request.getSession(true);
+			String message = "ログインしてください";
+			request.setAttribute("message", message);
+			RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+		}else {
+			Object loginCheck = session.getAttribute("login_id");
+			if (loginCheck == null){
+				String message = "ログインしてください";
+				request.setAttribute("message", message);
+				RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+				rd.forward(request, response);
+			} else {
+				String question = request.getParameter("question");
+				String[] answer = request.getParameterValues("answer");
 
-		if(isEmpty(question)){
-			//入力値が空だったら戻す
-			request.setAttribute("error_msg", "問題を入力してください");
-			RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
-			rd.forward(request, response);
-		}else if(question.length() > 500) {
-			request.setAttribute("error_msg", "問題の文字数が500文字を超えています");
-			RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
-			rd.forward(request,response);
-
-		}else{
-			//answerの配列の長さ分、ループ処理
-			for(int j=0;j<answer.length;j++){
-				if(answer[j].length() == 0) {
-					request.setAttribute("error_msg", "答えを入力してください");
+				if(isEmpty(question)){
+					//入力値が空だったら戻す
+					request.setAttribute("error_msg", "問題を入力してください");
 					RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
 					rd.forward(request, response);
-				//answerが200文字以上だった場合
-				}else if (answer[j].length() > 200) {
-					request.setAttribute("error_msg", "答えの文字数が200文字を超えています");
+				}else if(question.length() > 500) {
+					request.setAttribute("error_msg", "問題の文字数が500文字を超えています");
 					RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
 					rd.forward(request,response);
-				}else {
-					//入力値をsetAttribute()で登録
-					request.setAttribute("question", question);
-					request.setAttribute("answer", answer);
-					RequestDispatcher rd = request.getRequestDispatcher("registerConfirm.jsp");
-					rd.forward(request,response);
+
+				}else{
+					//answerの配列の長さ分、ループ処理
+					for(int j=0;j<answer.length;j++){
+						if(answer[j].length() == 0) {
+							request.setAttribute("error_msg", "答えを入力してください");
+							RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
+							rd.forward(request, response);
+							//answerが200文字以上だった場合
+						}else if (answer[j].length() > 200) {
+							request.setAttribute("error_msg", "答えの文字数が200文字を超えています");
+							RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
+							rd.forward(request,response);
+						}else {
+							//入力値をsetAttribute()で登録
+							request.setAttribute("question", question);
+							request.setAttribute("answer", answer);
+							RequestDispatcher rd = request.getRequestDispatcher("registerConfirm.jsp");
+							rd.forward(request,response);
+						}
+					}
 				}
 			}
 		}
