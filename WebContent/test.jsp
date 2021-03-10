80c4406bc6eef250ad1985363c561de893286801
httrhtm <h_hattori@emyus.jp>
2021-03-10 17:12:28 +0900

    aタグをformタグに統一。インデントを整理

--- WebContent/test.jsp
+++ WebContent/test.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ page import="bean.QuestionsBean" %>
+	pageEncoding="UTF-8"%>
+<%@ page import="bean.QuestionsBean"%>
 <%@ page import="java.util.ArrayList" import="java.util.List"%>
 <!DOCTYPE html>
 <html>
@@ -9,54 +9,56 @@
 <title>test</title>
 </head>
 <body>
-<!-- top, logoutボタン -->
-<div class ="nav">
-	<ul>
-		<li><a href="top.jsp">top</a>
-		<li><a href="logout">logout</a>
-		<br>
-	</ul>
-</div>
-<!-- ランダムで表示 -->
-<!-- 問題 -->
-<form action="TestResultServlet" method="post">
-<%
-//リストデータをリクエストから取得
-List<QuestionsBean> qlist = (List<QuestionsBean>)request.getAttribute("qlist");
+	<!-- top, logoutボタン -->
+	<div class="nav">
+		<form action="top.jsp" method="post">
+			<input type="submit" value="top">
+		</form>
+		<form action="LogoutServlet" method="post">
+			<input type="submit" value="logout">
+		</form>
+	</div>
+	<!-- ランダムで表示 -->
+	<!-- 問題 -->
+	<form action="TestResultServlet" method="post">
+		<%
+		//リストデータをリクエストから取得
+		List<QuestionsBean> qlist = (List<QuestionsBean>) request.getAttribute("qlist");
 
-	//questionのデータが空でない場合
-	if(qlist != null){
+		//questionのデータが空でない場合
+		if (qlist != null) {
 
-		//qestionのデータの数分、繰り返し処理
-		for(int i=0;i<qlist.size();i++){
+			//qestionのデータの数分、繰り返し処理
+			for (int i = 0; i < qlist.size(); i++) {
+		%>
+		<div class="inputQuestion">
+			<p>
+				<!-- 問題番号 -->
+				<input readonly type="text" name="questionId"
+					value="<%=qlist.get(i).getId()%>">
+			</p>
+			<p>
+				<!-- 問題 -->
+				<textarea readonly name="question"><%=qlist.get(i).getQuestion()%></textarea>
+			</p>
+		</div>
 
-%>
-<div class="inputQuestion">
-	<p>
-		<!-- 問題番号 -->
-		<input readonly type="text" name="questionId" value="<%= qlist.get(i).getId() %>">
-	</p>
-	<p>
-		<!-- 問題 -->
-		<textarea readonly name="question"><%= qlist.get(i).getQuestion() %></textarea>
-	</p>
-</div>
-
-<!-- 回答 -->
-<div class="input-answer">
-<p> 回答：
-	<!-- 答え -->
-		<input type="text" name="answer" id="answer">
-</p>
-</div>
-<%
-	}
-}
-%>
-<div class="bottomNav">
-	<input type="submit" value="採点">
-</div>
-</form>
+		<!-- 回答 -->
+		<div class="input-answer">
+			<p>
+				回答：
+				<!-- 答え -->
+				<input type="text" name="answer" id="answer">
+			</p>
+		</div>
+		<%
+		}
+		}
+		%>
+		<div class="bottomNav">
+			<input type="submit" value="採点">
+		</div>
+	</form>
 
 </body>
 </html>
--- src/servlet/TestServlet.java
+++ src/servlet/TestServlet.java
@@ -23,38 +23,36 @@ import dao.QuestionsDao;
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TestServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String error = "";
-
 		//HttpServletRequest.getSession()メソッドを呼び出しHttpSessionを取得
 		HttpSession session = request.getSession(false);
 		//sessionがnullだった場合、login画面へ遷移
 		if (session == null) {
 			session = request.getSession(true);
 			String message = "ログインしてください";
-        	request.setAttribute("message", message);
-        	RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+			request.setAttribute("message", message);
+			RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+			rd.forward(request, response);
 		}else {
-            Object loginCheck = session.getAttribute("login_id");
-            if (loginCheck == null){
-            	String message = "ログインしてください";
-            	request.setAttribute("message", message);
-            	RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+			Object loginCheck = session.getAttribute("login_id");
+			if (loginCheck == null){
+				String message = "ログインしてください";
+				request.setAttribute("message", message);
+				RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
 				rd.forward(request, response);
-            } else {
-
+			} else {
+				String error = "";
 				try {
 					//配列宣言
 					List<QuestionsBean> qlist = new ArrayList<QuestionsBean>();
@@ -72,15 +70,17 @@ public class TestServlet extends HttpServlet {
 					rd.forward(request, response);
 
 				}catch(SQLException e) {
-			        error ="DB接続エラーの為、一覧表示はできませんでした。";
-			        RequestDispatcher rd = request.getRequestDispatcher("top.jsp");
-			        rd.forward(request, response);
-			    }catch(Exception e){
-			        error ="予期せぬエラーが発生しました。<br>"+e;
-			        RequestDispatcher rd = request.getRequestDispatcher("top.jsp");
-			        rd.forward(request, response);
-			    }
-            }
+					error ="DB接続エラーの為、一覧表示はできませんでした。";
+					request.setAttribute("error", error);
+					RequestDispatcher rd = request.getRequestDispatcher("top.jsp");
+					rd.forward(request, response);
+				}catch(Exception e){
+					error ="予期せぬエラーが発生しました。<br>"+e;
+					request.setAttribute("error", error);
+					RequestDispatcher rd = request.getRequestDispatcher("top.jsp");
+					rd.forward(request, response);
+				}
+			}
 		}
 	}
 
